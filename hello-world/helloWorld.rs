fc9ce08e33eff728c97da12bf43345e16a9aaedd
kirito <karthik.ganti@mphasis.com>
2019-06-02 14:16:05 +0530

    Hello world

---
+++ hello-world/helloWorld.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello world!");
+
+    println!("{}, {}!", "hello", "world");
+    println!("{0}, {1}!", "hello", "world");
+
+    //println!("{greeting}, {name}!", greeting);
+}
