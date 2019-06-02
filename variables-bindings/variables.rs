57e8d0b62735afa77b48369d6beaaf9de2dddc40
kirito <karthik.ganti@mphasis.com>
2019-06-02 15:20:58 +0530

    variables

---
+++ variables-bindings/variables.rs
@@ -0,0 +1,25 @@
+
+
+fn main(){
+let a = true;
+
+let b: bool = true;
+
+let (x, y) = (1, 2);
+
+let _array = [1,2,3,4,5];
+println!("{}",a);
+
+println!("{}", x);
+
+println!("{}", _array[3]);
+
+
+
+const N: i64 = 5;
+const M: i32 = 5;
+
+println!("{}", N);
+println!("{}", M);
+//println!("{}"), _array[]);
+}
