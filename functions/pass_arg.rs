976a2abc579ab4784a3d0b730c4c0015c145a54d
kirito <karthik.ganti@mphasis.com>
2019-06-04 00:32:21 +0530

    functions

--- functions/pass_arg.rs
+++ functions/pass_arg.rs
@@ -5,6 +5,11 @@ fn print_sum(a: i8, b: i8){
 fn main(){
     print_sum(5,5);
     println!("{}", plus_one(10));
+
+    let test = plus_two;
+
+    let c = test(5);
+    println!("{}", c);
 }
 
 
@@ -19,3 +24,11 @@ fn plus_one(a: i32) -> i32 {
     return a + 1;
      
 }
+
+fn plus_two (a: i32) -> i32 {
+    return a + 2; //return
+}
+
+
+// function pointers, usage as a data type
+
